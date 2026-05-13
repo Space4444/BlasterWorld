307c62895ce0fa1894a9f1b67f9046723801e337
Alex <xbtc@ukr.net>
2026-05-13 22:32:19 +0300

    update

--- src/worker/auth.js
+++ src/worker/auth.js
@@ -2,40 +2,20 @@ import { betterAuth } from 'better-auth'
 import { env } from 'cloudflare:workers';
 
 
-function savePlayer(user) {
+async function savePlayer(user) {
     console.log('savePlayer');
     const { DB } = env;
     const u_id = user.id;
 
     console.log('saving player...');
-    DB.prepare("INSERT INTO players VALUES(?, 0, null, '0|1|1|1')")
+    await DB.prepare("INSERT INTO players VALUES(?, 0, null, '0|1|1|1')")
     .bind(u_id)
     .raw()
-    .then(res => {
-        console.log('player saved');
-    }, err => {
-        console.log('player saving error:', err);
-    })
-    .catch(err => {
-        console.log('player saving error 2:', err);
-    })
-    .finally(() => {
-        console.log('player saving finished');
-    });
-    DB.prepare('INSERT INTO items VALUES (?, 2, 1, 0, 0), (?, 3, 1, 1, 0), (?, 4, 1, 2, 0)')
+    console.log('player saved');
+    await DB.prepare('INSERT INTO items VALUES (?, 2, 1, 0, 0), (?, 3, 1, 1, 0), (?, 4, 1, 2, 0)')
     .bind(u_id, u_id, u_id)
-    .run()
-    .then(res => {
-        console.log('player items saved');
-    }, err => {
-        console.log('player items saving error:', err);
-    })
-    .catch(err => {
-        console.log('player items saving error 2:', err);
-    })
-    .finally(() => {
-        console.log('player items saving finished');
-    });;
+    .raw()
+    console.log('player items saved');
 }
 
 function hash(password) {
